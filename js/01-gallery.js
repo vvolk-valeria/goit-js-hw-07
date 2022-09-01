24a3e8b48ac589ad06f226533b131ea0b8a587cd
Volkova Valeria <vvolk.valeria@gmail.com>
2022-09-01 18:33:38 +0300

    Задание 2 - библиотека SimpleLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,14 +6,14 @@ import { galleryItems } from './gallery-items.js';
 const galleryEl = document.querySelector('.gallery');
 
 function galleryItemEl(arr) {
-  arr.map((item) => {
+  arr.map(({ preview, original, description }) => {
     galleryEl.insertAdjacentHTML('afterbegin', `<div class="gallery__item">
-  <a class="gallery__link" href='${item.original}'>
+  <a class="gallery__link" href='${original}'>
     <img
       class="gallery__image"
-      src='${item.preview}'
-      data-source='${item.original}'
-      alt='${item.description}'
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
     />
   </a>
 </div>`);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+
+const galleryEl = document.querySelector('.gallery');
+
+function galleryItemEl(arr) {
+  arr.map((item) => {
+      galleryEl.insertAdjacentHTML('afterbegin',
+          `<a class="gallery__item" href='${item.original}'>
+  <img class="gallery__image" src='${item.preview}' alt='${item.description}' />
+</a>`);
+  });
+}
+
+galleryItemEl(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
