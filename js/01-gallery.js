0c4ba3aece5aa184e37318493cf1cafd41f39e9b
Volkova Valeria <vvolk.valeria@gmail.com>
2022-09-01 16:52:05 +0300

    Задание 1 - галерея изображений

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,75 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function galleryItemEl(arr) {
+  arr.map((item) => {
+    galleryEl.insertAdjacentHTML('afterbegin', `<div class="gallery__item">
+  <a class="gallery__link" href='${item.original}'>
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>`);
+  });
+}
+
+galleryItemEl(galleryItems);
+
+galleryEl.addEventListener('click', onModalOpen);
+
+
+function onModalOpen(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG' ) {
+    return;   
+  }
+  
+  // создание модального окна 
+    const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}' width="1280" alt='${event.target.alt}'>
+`)
+  instance.show(); 
+
+  // закрытие модального окна с помощью клавиши Esc
+function onModalClose(event) {
+  if (event.code === 'Escape') {
+    instance.close();    
+  }
+}
+
+window.addEventListener('keydown', onModalClose);
+}
+
+
+  
+//= -----------------------------------------------------
+// const makeItemsEl = ({ preview, original, description }) => {
+//      return `<div class="gallery__item">
+//   <a class="gallery__link" href='${original}'>
+//     <img
+//       class="gallery__image"
+//       src='${preview}'
+//       data-source='${original}'
+//       alt='${description}'
+//     />
+//   </a>
+// </div>`;
+
+// };
+// console.log(makeItemsEl);   
+
+// const createItemsEl = galleryItems.map(makeItemsEl).join("");
+
+// console.log(createItemsEl); 
+
+// galleryEl.insertAdjacentHTML('afterbegin', createItemsEl);
+//= -----------------------------------------------------
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,4 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
